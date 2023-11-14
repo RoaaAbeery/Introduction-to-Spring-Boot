ab459cba01a36e7bb15a216784f7b9d4e4b1bd56
RoaaAbeery <148685929+RoaaAbeery@users.noreply.github.com>
2023-11-14 12:59:58 +0300

    Add files via upload

---
+++ exercise1/Exercise1Application.java
@@ -0,0 +1,15 @@
+package com.example.exercise1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+
+public class Exercise1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exercise1Application.class, args);
+    }
+
+}
---
+++ exercise1/welcomeController.java
@@ -0,0 +1,32 @@
+package com.example.exercise1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+public class welcomeController {
+    @GetMapping("/name")
+    public String name(){
+        return "My name is roaa";
+    }
+    @GetMapping("/age")
+    public String age (){
+        return "My age is 23";
+    }
+    @GetMapping("check/status")
+public String check (){
+        return "Everything OK";
+}
+    @GetMapping("/health")
+public String health(){
+        return "Server health is up and running";
+}
+@GetMapping("/names")
+public String[] names(){
+        String [] n={"amal","Roaa", "Abrar"};
+
+        return n;
+}
+}
